8eb5d7b40419d62ddfc5c6450b9909ca4648e29d
augustelofsson <55317290+augustelofsson@users.noreply.github.com>
2020-03-27 15:55:22 +0100

    initial

---
+++ src/Context/BidContext.js
@@ -0,0 +1,35 @@
+import React,{createContext, useState} from 'react'
+
+
+
+const API_URL = 'http://nackowskis.azurewebsites.net/api/';
+const GROUP_NUM = 2230;
+const url = "http://nackowskis.azurewebsites.net/api/bud/32/1"
+const BidContext = createContext();
+const test1 = "";
+
+
+
+export const BidProvider = (props) =>  {
+
+    const [bids, setBids] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+
+
+    const GetBids = async (id) => {
+        fetch(`${API_URL}bud/${GROUP_NUM}/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            setIsLoading(false);
+            setBids(data);
+        });
+    }
+
+    const AddBid = async (amount) => {
+                  
+    }
+
+
+
+}
